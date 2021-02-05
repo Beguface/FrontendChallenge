4d002a6f0168c1222209a7e2d36dcd8af44a0416
beguface <beguface@gmail.com>
2021-02-04 22:13:42 -0400

    Formik and yup validation added

--- components/SearchBar/SearchBar.js
+++ components/SearchBar/SearchBar.js
@@ -8,25 +8,36 @@ import {
   DrawerContent,
   DrawerFooter,
   DrawerOverlay,
+  FormControl,
+  FormErrorMessage,
   Input,
   InputGroup,
   InputLeftElement,
+  SimpleGrid,
   useDisclosure,
 } from "@chakra-ui/react";
 import { SearchIcon, SettingsIcon } from "@chakra-ui/icons";
-import { useForm } from "@hooks/useForm";
 import Router from "next/router";
+import { Field, Form, Formik } from "formik";
+import * as Yup from "yup";
 
 const SearchBar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [{ term, location }, handleInputChange, formReset] = useForm({
-    term: "",
-    location: "",
-  });
+
   const buttonRef = React.useRef();
 
-  const handleOnSubmit = (e) => {
-    e.preventDefault();
+  const initalValues = {
+    term: "",
+    location: "",
+  };
+
+  const validationSchema = Yup.object({
+    term: Yup.string().required("termino requerido"),
+    location: Yup.string().required("Ubicacion requerida"),
+  });
+
+  const handleOnSubmit = (values, actions) => {
+    const { term, location } = values;
     Router.push({
       pathname: "/search",
       query: {
@@ -34,7 +45,7 @@ const SearchBar = () => {
         location: location,
       },
     });
-    formReset();
+    actions.resetForm();
     onClose();
   };
 
@@ -65,45 +76,87 @@ const SearchBar = () => {
         <DrawerOverlay>
           <DrawerContent>
             <DrawerCloseButton />
-            <form onSubmit={handleOnSubmit}>
-              <DrawerBody mt="50px">
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents="none"
-                    children={<SearchIcon color="gray.300" />}
-                  />
-                  <Input
-                    type="text"
-                    autoComplete="off"
-                    name="term"
-                    value={term}
-                    onChange={handleInputChange}
-                    placeholder="Pizza, hamburguesas, hot dog..."
-                  />
-                </InputGroup>
-                <InputGroup mt="4">
-                  <InputLeftElement
-                    pointerEvents="none"
-                    children={<SettingsIcon color="gray.300" />}
-                  />
-                  <Input
-                    type="text"
-                    autoComplete="off"
-                    name="location"
-                    value={location}
-                    onChange={handleInputChange}
-                    placeholder="Algun lugar..."
-                  />
-                </InputGroup>
-              </DrawerBody>
+            <Formik
+              initialValues={initalValues}
+              validationSchema={validationSchema}
+              onSubmit={handleOnSubmit}
+            >
+              {({ isSubmitting }) => (
+                <Form>
+                  <DrawerBody mt="50px">
+                    <SimpleGrid columns={[1, 2]} spacing={[1, 10]}>
+                      <Field name="term">
+                        {({ field, form }) => (
+                          <FormControl
+                            isInvalid={form.touched.term && form.errors.term}
+                          >
+                            <InputGroup>
+                              <InputLeftElement
+                                pointerEvents="none"
+                                children={<SearchIcon color="gray.300" />}
+                              />
+                              <Input
+                                {...field}
+                                type="text"
+                                autoComplete="off"
+                                name="term"
+                                placeholder="Pizza, hamburguesas, hot dog..."
+                              />
+                            </InputGroup>
+                            {form.touched.term && form.errors.term ? (
+                              <FormErrorMessage fontSize="xs">
+                                {form.errors.term}
+                              </FormErrorMessage>
+                            ) : (
+                              <Box height="25px" />
+                            )}
+                          </FormControl>
+                        )}
+                      </Field>
 
-              <DrawerFooter>
-                <Button variant="outline" mr={3} onClick={onClose}>
-                  Volver
-                </Button>
-                <Button type="submit">Buscar</Button>
-              </DrawerFooter>
-            </form>
+                      <Field name="location">
+                        {({ field, form }) => (
+                          <FormControl
+                            isInvalid={
+                              form.touched.location && form.errors.location
+                            }
+                          >
+                            <InputGroup>
+                              <InputLeftElement
+                                pointerEvents="none"
+                                children={<SettingsIcon color="gray.300" />}
+                              />
+                              <Input
+                                {...field}
+                                type="text"
+                                autoComplete="off"
+                                name="location"
+                                placeholder="Algún lugar cercano..."
+                              />
+                            </InputGroup>
+                            {form.touched.location && form.errors.location ? (
+                              <FormErrorMessage fontSize="xs">
+                                {form.errors.location}
+                              </FormErrorMessage>
+                            ) : (
+                              <Box height="25px" />
+                            )}
+                          </FormControl>
+                        )}
+                      </Field>
+                    </SimpleGrid>
+                  </DrawerBody>
+                  <DrawerFooter>
+                    <Button variant="ghost" mr={3} onClick={onClose}>
+                      Volver
+                    </Button>
+                    <Button type="submit" isLoading={isSubmitting}>
+                      Buscar
+                    </Button>
+                  </DrawerFooter>
+                </Form>
+              )}
+            </Formik>
           </DrawerContent>
         </DrawerOverlay>
       </Drawer>
--- pages/index.js
+++ pages/index.js
@@ -4,56 +4,9 @@ import CarruselContainer from "@components/Containers/CarruselContainer";
 export default function Home() {
   return (
     <>
-      <Box>
+      <Box height="calc(100vh - 175px)">
         <CarruselContainer />
       </Box>
     </>
   );
 }
-
-// export async function getServerSideProps(ctx) {
-//   const apolloClient = initializeApollo();
-//   let businesses = [],
-//     error;
-//   let term = "pizza";
-//   let location = "san francisco";
-
-//   try {
-//     const { data } = await apolloClient.query({
-//       query: gql`
-//         query GetBusiness($term: String!, $location: String!) {
-//           search(term: $term, location: $location, limit: 10) {
-//             total
-//             business {
-//               id
-//               photos
-//               name
-//               location {
-//                 address1
-//                 city
-//                 state
-//                 postal_code
-//                 country
-//                 formatted_address
-//               }
-//               review_count
-//               rating
-//               display_phone
-//               phone
-//             }
-//           }
-//         }
-//       `,
-//       variables: { term, location },
-//     });
-//     businesses = data.search.business;
-//   } catch (e) {
-//     error = e;
-//   }
-
-//   return {
-//     props: {
-//       businesses,
-//     },
-//   };
-// }
